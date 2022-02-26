b19bb310f5ec66436ccbb01a1de373744da47d22
CozyBrian <dragonbrian33@gmail.com>
2022-02-26 00:15:19 +0000

    feat: timer

--- src/components/Timer/timer.tsx
+++ src/components/Timer/timer.tsx
@@ -1,19 +1,52 @@
 import React, {useState} from "react";
 import { colors } from "../../constants";
+import { Vibration, Platform } from "react-native";
 import styled from "styled-components";
+import {ProgressBar} from "react-native-paper";
+import { Timing } from "./timing";
+import { Button } from "../../utils/Button";
 import { Countdown } from "./countdown";
 
 const Container = styled.View`
   flex: 1;
 `;
-
 const CountdownView = styled.View`
-
+  flex: 0.5;
+  justify-content: center;
+`;
+const ButtonWrapper = styled.View`
+  flex: 0.3;
+  flex-direction: row;
+  padding: 16px;
+  justify-content: center;
+  align-items: center;
+`;
+const SubjectView = styled.View`
+  padding-top: 20px;
+`;
+const FText = styled.Text`
+  color: ${colors.White};
+  text-align: center;
+`;
+const SText = styled.Text`
+  color: ${colors.White};
+  text-align: center;
+  font-weight: bold;
+`;
+const ClearView = styled.View`
+  padding-bottom: 20px;
+  padding-left: 20px;
 `;
 
 const DEFAULT_TIME = 0.1;
 
-export const Timer = () => {
+type Props = {
+  focusSubject: string | null,
+  clearSubject: () => void,
+  onTimerEnd: () => void,
+}
+
+export const Timer = ({focusSubject, clearSubject, onTimerEnd}:Props) => {
   const [minutes, setMinutes] = useState<number>(DEFAULT_TIME);
   const [isStarted, setIsStarted] = useState<boolean>(false);
   const [progress, setProgress] = useState<number>(1);
@@ -22,18 +55,52 @@ export const Timer = () => {
     setProgress(progress);
   };
 
+  const vibrate = () => {
+    if (Platform.OS === 'ios') {
+      const interval = setInterval(() => Vibration.vibrate(), 1000);
+      setTimeout(() => clearInterval(interval), 10000);
+    } else {
+      Vibration.vibrate(2000);
+    }
+  };
+
   const onEnd = () => {
+    vibrate();
     setMinutes(DEFAULT_TIME);
     setProgress(1);
     setIsStarted(false);
-    //onTimerEnd();
+    onTimerEnd();
+  };
+
+  const changeTime = (min: number) => {
+    setMinutes(min);
+    setProgress(1);
+    setIsStarted(false);
   };
 
   return (
     <Container>
       <CountdownView>
         <Countdown minutes={minutes} onProgress={onProgress} onEnd={onEnd} isPaused={!isStarted} />
+        <ProgressBar progress={progress} color={colors.White} style={{height: 7.5}} />
       </CountdownView>
+      <SubjectView>
+        <FText>Focusing on:</FText>
+        <SText>{focusSubject}</SText>
+      </SubjectView>
+      <ButtonWrapper>
+        <Timing onChangeTime={changeTime}/>
+      </ButtonWrapper>
+      <ButtonWrapper>
+        {isStarted ? (
+          <Button title="pause" size={80} onPress={() => setIsStarted(false)}/>
+        ) : ( 
+          <Button title="play" size={80} onPress={() => setIsStarted(true)}/>
+        )}
+      </ButtonWrapper>
+      <ClearView>
+        <Button title="stop" onPress={() => clearSubject()}/>
+      </ClearView>
     </Container>
   )
 }
---
+++ src/components/Timer/timing.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from '../../constants';
+import { Button } from '../../utils/Button';
+
+const ButtonView = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+
+type Props = {
+  onChangeTime: (min: number) => void,
+}
+
+export const Timing = ({onChangeTime}: Props) => {
+  return (
+    <>
+    <ButtonView>
+      <Button title="10" bc={colors.Secondary} onPress={() => onChangeTime(10)}/>
+    </ButtonView>
+    <ButtonView>
+      <Button title="15" bc={colors.Secondary} onPress={() => onChangeTime(15)}/>
+    </ButtonView>
+    <ButtonView>
+      <Button title="20" bc={colors.Secondary} onPress={() => onChangeTime(20)}/>
+    </ButtonView>
+    </>
+  )
+}
