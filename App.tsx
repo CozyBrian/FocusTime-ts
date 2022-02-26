3dcb29a07aa468d0f8ad0889bb587a6d6f1841a9
CozyBrian <dragonbrian33@gmail.com>
2022-02-26 00:35:42 +0000

    feat: save focus history with asyncstorage

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { Focus } from './src/components/Focus/focus';
 import { FocusType } from './src/components/modal';
 import { Timer } from './src/components/Timer/timer';
 import { FocusHistory } from './src/components/Focus/focusHistory';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const STATUSES = {
   COMPLETE: 1,
@@ -27,6 +28,32 @@ export default function App() {
     setFocusSubject(null);
   }
 
+  const saveFocusHistory = async () => {
+    try {
+      AsyncStorage.setItem('focusHistory', JSON.stringify(focusHistory));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const loadFocusHistory = async () => {
+    try {
+      const history = await AsyncStorage.getItem('focusHistory');
+      if (history && JSON.parse(history).length) {
+        setFocusHistory(JSON.parse(history));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadFocusHistory();
+  }, []);
+
+  useEffect(() => {
+    saveFocusHistory();
+  }, [focusHistory]);
 
 
   return (
